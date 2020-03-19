9ad1d1ad882c369125c5079287b1e3ed8eaff9df
nonmcw <nonmcw@126.com>
2020-03-19 17:05:31 +0800

    add sidebar html file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 from config import config
 from app.views import views as views_blueprint
 from app.extensions import bootstrap, mail, moment, db, ckeditor
+from app.models import Admin, Category
 
 def create_app(config_name=None):
     if config_name == None:
@@ -43,7 +44,11 @@ def register_shell_context(app):
         return dict(db=db)
     
 def register_template_context(app):
-    pass
+    @app.context_processor
+    def make_template_context():
+        admin = Admin.query.frist()
+        categories = Category.query.order_by(Category.name).all()
+        return dict(admin=admin, categories=categories)
 
 def register_errors(app):
     @app.errorhandler(400)
